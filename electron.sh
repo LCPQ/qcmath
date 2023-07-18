a05af9a67be46447620ca21f184a50939c629f86
EnzoMonino <enzo.monino@irsamc.ups-tlse.fr>
2023-07-18 11:57:34 +0200

    add and clean qsGT

--- electron.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-./qcaml-tools/qcmath_input -b basis/$1 -x mol/mol.xyz -m $2 -c $3
-
--- integrals.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-./qcaml-tools/qcmath_integrals -b basis/$1 -x mol/mol.xyz 
-
-#./qcaml-tools/GoMath -b basis/$1 -x mol/mol.xyz
